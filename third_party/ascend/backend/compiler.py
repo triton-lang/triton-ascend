d37441a83eaf57896b5c5a328dd86ed845c88bd0
Sky_miner <liuyichen23@huawei.com>
2026-05-24 16:16:57 +0800

    feat(ssbuffer): Enable the compilation option `enable_dynamic_cv_pipeline` by default.

--- third_party/ascend/backend/compiler.py
+++ third_party/ascend/backend/compiler.py
@@ -951,7 +951,7 @@ class NPUOptions:
     enable_vf_fusion: bool = None
     # todo: this code will be removed in version 530.
     add_auto_scheduling: bool = False
-    enable_dynamic_cv_pipeline: bool = False
+    enable_dynamic_cv_pipeline: bool = True if is_compile_on_910_95 else False
     hfusion_enable_multiple_consumer_fusion: bool = False
     has_auto_blockify_blacklist_op: Optional[bool] = None
     intra_cache_num: int = None

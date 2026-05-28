984827dfed895025f324d2719ee6a363076c9b83
Andrey <volkov.andrey@huawei.com>
2026-05-28 17:06:33 +0300

    linter checks

--- third_party/ascend/lib/TritonToStructured/TritonToStructuredPass.cpp
+++ third_party/ascend/lib/TritonToStructured/TritonToStructuredPass.cpp
@@ -148,7 +148,6 @@ void TritonToStructuredPass::runOnOperation() {
   }
 
   PassManager pm(&getContext(), moduleOp.getOperationName());
-  
   //Disable optimizations for the Debug mode
   if (!::triton::tools::getBoolEnv("TRITON_DEBUG")) {
     pm.addPass(createCSEPass());

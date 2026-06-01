0bc6f81f4dc20c83584b58ade7b8a6a159b8b44f
Four1er <245516766@qq.com>
2026-06-01 07:56:05 +0000

    fix(ssbuffer): Add more specific kernels.

--- third_party/ascend/lib/DynamicCVPipeline/AnalyzeDataFlow/AnalyzeName.cpp
+++ third_party/ascend/lib/DynamicCVPipeline/AnalyzeDataFlow/AnalyzeName.cpp
@@ -53,6 +53,9 @@ static constexpr llvm::StringLiteral interceptrFunc[] {
   "bwd_qkv_kernel",
   "parallel_rebased_bwd_kernel",
   "parallel_rebased_fwd_kernel",
+  "parallel_path_bwd_dkv_kernel",
+  "chunkwise_bwd_kernel_hdqgl",
+  "parallel_simple_gla_bwd_kernel",
 };
 
 static LogicalResult verifyFuncNames(ModuleOp module)

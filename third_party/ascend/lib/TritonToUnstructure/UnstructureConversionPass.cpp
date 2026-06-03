40c941edca86228d82650fca141519ef8729b52e
Andrey <volkov.andrey@huawei.com>
2026-06-03 11:13:30 +0300

    Update UnstructureConversionPass.cpp

--- third_party/ascend/lib/TritonToUnstructure/UnstructureConversionPass.cpp
+++ third_party/ascend/lib/TritonToUnstructure/UnstructureConversionPass.cpp
@@ -728,7 +728,7 @@ void TritonToUnstructurePass::runOnOperation() {
 
   PassManager pm(&getContext(), moduleOp.getOperationName());
   // Disable optimizations for the Debug mode
-  if (!::triton::tools::getBoolEnv("TRITON_DEBUG")) {
+  if (!::triton::tools::getBoolEnv("LLVM_EXTRACT_DI_LOCAL_VARIABLES")) {
     pm.addPass(createCSEPass());
     pm.addPass(createCanonicalizerPass());
   }

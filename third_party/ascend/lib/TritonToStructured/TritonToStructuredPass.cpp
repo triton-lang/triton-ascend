4bef288defee13b2b217200a9d66efb2032f7fd7
Andrey <volkov.andrey@huawei.com>
2026-06-03 11:12:04 +0300

    Update TritonToStructuredPass.cpp

--- third_party/ascend/lib/TritonToStructured/TritonToStructuredPass.cpp
+++ third_party/ascend/lib/TritonToStructured/TritonToStructuredPass.cpp
@@ -149,7 +149,7 @@ void TritonToStructuredPass::runOnOperation() {
 
   PassManager pm(&getContext(), moduleOp.getOperationName());
   // Disable optimizations for the Debug mode
-  if (!::triton::tools::getBoolEnv("TRITON_DEBUG")) {
+  if (!::triton::tools::getBoolEnv("LLVM_EXTRACT_DI_LOCAL_VARIABLES")) {
     pm.addPass(createCSEPass());
     pm.addPass(createCanonicalizerPass());
   }

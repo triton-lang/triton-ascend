bc0f44b27decb38bcdbf68c1b9f555d5df16d45d
dingyi <dingyi@huawei.com>
2026-06-24 12:42:15 +0800

    [ssbuffer](fix): avoid unsigned underflow in condition use check

--- third_party/ascend/lib/DynamicCVPipeline/AnalyzeDataFlow/AnalyzeName.cpp
+++ third_party/ascend/lib/DynamicCVPipeline/AnalyzeDataFlow/AnalyzeName.cpp
@@ -46,7 +46,6 @@ static constexpr llvm::StringLiteral interceptrFunc[] {
     "bwd_qkv_kernel",
     "parallel_nsa_compression_fwd_kernel",
     "parallel_nsa_compression_bwd_kernel_dq",
-    "chunk_dplr_fwd_kernel_h",
 };
 
 static LogicalResult verifyFuncNames(ModuleOp module)

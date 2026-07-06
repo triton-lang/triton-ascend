79c03bf7712cd34ed6d4a052b1fb3a9f277fb3f7
Andrey <volkov.andrey@huawei.com>
2026-07-06 10:18:50 +0300

    Update OptimizeDotOperands.cpp

--- lib/Dialect/TritonGPU/Transforms/OptimizeDotOperands.cpp
+++ lib/Dialect/TritonGPU/Transforms/OptimizeDotOperands.cpp
@@ -324,7 +324,7 @@ public:
 
     OpPassManager pm;
     // Disable optimizations for the Debug mode
-    if (!triton::tools::getBoolEnv("LLVM_EXTRACT_DI_LOCAL_VARIABLES")) {
+    if (!triton::tools::getBoolEnv("TRITON_DISABLE_OPTIMIZATIONS")) {
       pm.addPass(mlir::createCanonicalizerPass());
     }
     if (failed(runPipeline(pm, m)))

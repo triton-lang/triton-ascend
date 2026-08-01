9614378bd6189ef9ba1fe2f64fe143ecd1dcc3b3
wutianyao <wutianyao1@huawei.com>
2026-08-01 17:46:36 +0800

    [triton-dist](feat) Add triton-dist in triton-ascend as submodule

--- third_party/ascend/backend/compiler.py
+++ third_party/ascend/backend/compiler.py
@@ -35,7 +35,10 @@ from typing import Any, Dict, Optional, Tuple, Union
 
 from triton._C.libtriton import ir, passes, ascend, buffer_ir
 from triton._C.libtriton.ascend import ir as ascend_ir
-
+try:
+    from triton._C.libtriton import distributed
+except ImportError:
+    distributed = None
 from triton.backends.ascend.utils import (
     _check_bishengir_api_change,
     _check_bishengir_able_save_ir,
@@ -191,6 +194,8 @@ def ttir_to_linalg(mod, metadata, opt, *, named_ops=False):
             auto_blockify_size = 1
         pm = ir.pass_manager(mod.context)
         pm.enable_debug()
+        if distributed is not None:
+            distributed.ascend_passes.ttgpuir.add_convert_triton_distributed_to_hivm(pm)
         # ascend.passes.ttir.add_auto_blockify(pm, auto_blockify_size)
 
         ascend.passes.ttir.add_triton_control_flow_opt(pm)
@@ -1265,6 +1270,8 @@ class AscendBackend(BaseBackend):
     def load_dialects(self, ctx):
         from triton._C.libtriton import buffer_ir
         from triton._C.libtriton.ascend import ir as ascend_ir
+        if distributed is not None:
+            distributed.load_dialects(ctx)
         buffer_ir.load_dialects(ctx)
         ascend_ir.load_dialects(ctx)
         ascend.load_dialects(ctx)

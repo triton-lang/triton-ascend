9dcf3f19ed3440683961420b6005cec1bc344574
chenwuyang <chenwuyang1@huawei.com>
2026-08-12 11:47:48 +0800

    fix reviews

--- third_party/ascend/lib/DynamicCVPipeline/PlanComputeBlock/PlanVectorBlockPass.cpp
+++ third_party/ascend/lib/DynamicCVPipeline/PlanComputeBlock/PlanVectorBlockPass.cpp
@@ -220,11 +220,13 @@ collectKeepOps(Block *block, SmallVector<Operation *> toProcess,
     }
     keepOps.insert(op);
 
-    depHelper.forEachSource<true>(op, [&](Operation *source) {
-      if (!keepOps.contains(source) && llvm::is_contained(fuseGroup, source)) {
-        toProcess.push_back(source);
-      }
-    });
+    depHelper.forEachSource<DependencyHelper::SourceMode::AcrossIterArg>(
+        op, [&](Operation *source) {
+          if (!keepOps.contains(source) &&
+              llvm::is_contained(fuseGroup, source)) {
+            toProcess.push_back(source);
+          }
+        });
   }
 
   // special case: annotation.mark always follows the defining op
@@ -551,7 +553,8 @@ void PlanVectorBlockPass::runOnOperation() {
     return WalkResult::advance();
   });
   if (result.wasInterrupted()) {
-    signalPassFailure();
+    LOG_DEBUG("Failed to plan vector block id for block\n");
+    CVPipeline::setFallbackAttr(moduleOp, CVPipeline::ERRCODE_FAILED);
   }
 }
 

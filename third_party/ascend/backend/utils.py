b82ba25806aa42f912c797fa98b263e8082e816a
zhanwei33 <22679270+zhanwei33@users.noreply.github.com>
2026-08-23 16:24:41 +0800

    [compiler] describe deprecated enable_select_analysis option

--- third_party/ascend/backend/utils.py
+++ third_party/ascend/backend/utils.py
@@ -42,7 +42,6 @@ _DEPRECATED_NPU_OPTIONS = frozenset({
     "arch",
     "compile_on_910_95",
     "enable_costmodel_backend",
-    "enable_select_analysis",
     "enable_sync_block_lock",
     "enable_ub_refine_opt",
     "enable_vf_fusion",

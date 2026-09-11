50b89868eb7d549c72c44a535b401794102d0602
m-everglow <127195481+m-everglow@users.noreply.github.com>
2026-09-11 15:30:46 +0800

    [ssbuffer](fix) adjust iter_args insertion position in whileOp (#2015)
    Signed-off-by: m-everglow <2276518549@qq.com>

--- third_party/ascend/lib/DynamicCVPipeline/AnalyzeDataFlow/AnalyzeName.cpp
+++ third_party/ascend/lib/DynamicCVPipeline/AnalyzeDataFlow/AnalyzeName.cpp
@@ -45,7 +45,6 @@ static constexpr llvm::StringLiteral interceptrFunc[]{
     "kernel_da_bwd_kv_ul",
     "pcb06_tc02_c2v2v2c_chain",
     "flash_varlen_fwd_kernel",
-    "_jagged_flash_attention_bwd_basic_kernel",
     "_sparse_decode_kernel",
     "_sparse_decode_model1_kernel",
     "sparse_flash_attention_grad_kernel",

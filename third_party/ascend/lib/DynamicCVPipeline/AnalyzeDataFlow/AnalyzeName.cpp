7860206ccab956d5b591f220768dcaa1b6f9a1f4
Shao Yiyang <57837045+Hsyy04@users.noreply.github.com>
2026-09-11 21:40:22 +0800

    [ssbuffer] (fix) ssbuffer Add new known NoMemoryEffectCall (#2032)
    Co-authored-by: shaoyiyang <shaoyiyang1@huawei.com>

--- third_party/ascend/lib/DynamicCVPipeline/AnalyzeDataFlow/AnalyzeName.cpp
+++ third_party/ascend/lib/DynamicCVPipeline/AnalyzeDataFlow/AnalyzeName.cpp
@@ -48,9 +48,9 @@ static constexpr llvm::StringLiteral interceptrFunc[]{
     "_sparse_decode_kernel",
     "_sparse_decode_model1_kernel",
     "sparse_flash_attention_grad_kernel",
-    "parallel_path_fwd_kernel",
     "flex_attention_backward_dkdv_kernel",
     "flex_attention_backward_dkdv_kernel_tasklist",
+    "chunkwise_bwd_kernel_dhg",
 };
 
 static LogicalResult verifyFuncNames(ModuleOp module) {

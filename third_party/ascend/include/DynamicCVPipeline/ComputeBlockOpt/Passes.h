5ded5c4e01a91c4310a2d3f98ef666cdaf9777cf
zhanwei33 <493694187@qq.com>
2026-09-17 17:15:34 +0800

    Revert "[ssbuffer](feat) optimization for load-exp pattern (#2133)"
    This reverts commit 4fef68e0faa53228bb452af1a25f0d66246a0b67.

--- third_party/ascend/include/DynamicCVPipeline/ComputeBlockOpt/Passes.h
+++ third_party/ascend/include/DynamicCVPipeline/ComputeBlockOpt/Passes.h
@@ -56,7 +56,6 @@ createMergeInputInitSharedCubeBlockPass();
 
 std::unique_ptr<OperationPass<ModuleOp>> createSplitIfByBlockIdPass();
 std::unique_ptr<OperationPass<ModuleOp>> createExpSubfPatternPass();
-std::unique_ptr<OperationPass<ModuleOp>> createExpLoadPatternPass();
 void registerSplitIfByBlockIdPass();
 
 } // namespace triton
